ea8c625cc56459861f41838c2a95dcbd053a4751
unknown <mrudhu2306@gmail.com>
2026-03-02 18:19:29 +0530

    Initial commit for weekly assignments

---
+++ week 1/day1ass2.js
@@ -0,0 +1,14 @@
+//find the big number in three ways
+let c=5;
+if(a>b&&a>c)
+{
+    console.log(a);
+}
+else if(b>c&&b>a)
+{
+    console.log(b);
+}
+else 
+{
+    console.log(c);
+}
---
+++ week 1/day2ass1.js
@@ -0,0 +1,40 @@
+const employees = [
+  {
+    eno: 101,
+    name: "Ravi",
+    marks: [78, 82, 91],
+  },
+  {
+    eno: 102,
+    name: "Bhanu",
+    marks: [65, 70, 68],
+  },
+  {
+    eno: 103,
+    name: "Sneha",
+    marks: [88, 92, 95],
+  },
+  {
+    eno: 104,
+    name: "Kiran",
+    marks: [55, 60, 58],
+  },
+  {
+    eno: 105,
+    name: "Anitha",
+    marks: [90, 85, 87],
+  },
+];
+// Insert new Emp at 2nd position
+employees.splice(2,0,{ eno: 106, name: "neetha",marks: [90, 85, 37] })
+console.log(employees)
+//remove emp with name "Kiran"
+
+let index = employees.findIndex(emp => emp.name === "Kiran");
+employees.splice(index, 1);
+console.log(employees)
+//Change the last mark 95 to 75 of emp  "Sneha"
+
+let result = employees.find(emp => emp.name === "Sneha");
+result.marks.splice(2,1,75)
+console.log(employees)
